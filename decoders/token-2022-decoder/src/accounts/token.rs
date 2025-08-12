f5c91d3b61df248adcc56dc2adee7a12e267d11f
peng-huang-ch <h499871809@gmail.com>
2025-08-12 18:25:37 +0800

    refactor: remove discriminator from Token struct in token.rs

--- decoders/token-2022-decoder/src/accounts/token.rs
+++ decoders/token-2022-decoder/src/accounts/token.rs
@@ -6,7 +6,6 @@ use {
 #[derive(
     CarbonDeserialize, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq, Clone, Hash,
 )]
-#[carbon(discriminator = "0xc971463ece9dd1b2")]
 pub struct Token {
     pub mint: solana_pubkey::Pubkey,
     pub owner: solana_pubkey::Pubkey,
